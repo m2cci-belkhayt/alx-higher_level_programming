0bb15ddea1f69bb059e3494988264579805e7e93
tbelkhayate <belkhayatetaha1@gmail.com>
2023-08-17 23:18:57 +0200

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
     max_val = 0
+    max_key = None
     for k in a_dictionary:
         if max_val < a_dictionary[k]:
             max_val = a_dictionary[k]
-    return max_val
+            max_key = k
+    return max_key
