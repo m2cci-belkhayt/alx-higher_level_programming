585377c0876b68964eee4dd4c5fe3938d0613b3a
tbelkhayate <belkhayatetaha1@gmail.com>
2023-08-18 00:11:15 +0200

    100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    else:
+        dominateur = 0
+        summ = 0
+        for i in range(0, len(my_list)):
+            summ += my_list[i][2] * my_list[i][1]
+            dominateur += my_list[i][2]
+        return (summ / dominateur)
