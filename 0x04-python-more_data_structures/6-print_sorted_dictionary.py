40fb4bb103eff70f2b56e9533c168a5685c7b544
tbelkhayate <belkhayatetaha1@gmail.com>
2023-08-17 09:45:46 +0200

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-
+    """
+    Returns a set of all elements present in only one set
+    ...
+    Parameters
+    ----------
+    set_1 : set
+    first set of elements
+    set_2 : set
+    second set of elements
+    Return:
+    the result of the operation (&)
+    """
+    
     return(sorted(a_dictionary.keys()))
