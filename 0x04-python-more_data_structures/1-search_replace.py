f32f7e169c3f5ddcf024c8343bbe0aab7343073c
tbelkhayate <belkhayatetaha1@gmail.com>
2023-08-17 09:02:48 +0200

    changes in task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(0, len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+    result = []
+    for item in my_list:
+        if item == search:
+            result.append(replace)
+        else:
+            result.append(item)
+    return result
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# alx-higher_level_programming
-Now we get to the advanced level of programing, programing with python
+Python - More Data Structures: Set, Dictionary
