c30c97711a26b6cdf52e783f1138172ae51aeb7f
tbelkhayate <belkhayatetaha1@gmail.com>
2023-09-13 23:27:02 +0200

    task 1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Module 0-lookup
-List of available attributes & methods of an object
+"""Module of a class 0-lookup
+Finding a list of available attributes & methods of an object
 """
 
+
 def lookup(obj):
-"""Returns a list of attributes and methods
-Args:
-- obj: object to look into
-"""
+    """Returns that list of attributes and methods
+    Args:
+    - obj: object to look into
+    """
 
     return dir(obj)
