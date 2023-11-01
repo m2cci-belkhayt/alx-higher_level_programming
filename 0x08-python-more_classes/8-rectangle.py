754a6094ac575b54d875a6dc63b5dec54181e9a8
tbelkhayate <belkhayatetaha1@gmail.com>
2023-11-01 18:33:28 +0100

    task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -8,25 +8,14 @@
 
 
 class Rectangle:
+    """ rectangle object with getters and setters"""
     number_of_instances = 0
     print_symbol = "#"
 
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-        elif value < 0:
-            raise ValueError('width must be >= 0')
-        self.__width = value
 
     @property
     def height(self):
@@ -40,24 +29,41 @@ class Rectangle:
             raise ValueError('height must be >= 0')
         self.__height = value
 
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
     def area(self):
-        return self.__width * self.__height
+        if (self.height == 0 or self.width == 0):
+            return 0
+        else:
+            return (self.height * self.width)
 
     def perimeter(self):
-        return 2 * (self.__width + self.__height)
+        if (self.height == 0 or self.width == 0):
+            return 0
+        else:
+            return 2 * (self.height + self.width)
 
     def __str__(self):
-        if self.__width == 0 or self.__height == 0:
+        if (self.height == 0 or self.width == 0):
             return ""
         else:
             rect_str = ""
             for _ in range(self.__height):
-                rect_str += str(Rectangle.print_symbol) * self.__width + "\n"
+                rect_str += str(self.print_symbol) * self.__width + "\n"
             return rect_str[:-1]
 
-    def __del__(self):
-        Rectangle.number_of_instances -= 1
-        print("Bye rectangle...")
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
