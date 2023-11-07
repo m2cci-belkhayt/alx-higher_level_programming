b7011899bc45c3dfb0488e7eeb35ccf405580a4f
tbelkhayate <belkhayatetaha1@gmail.com>
2023-11-07 19:13:00 +0100

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+reads the content of a text file (UTF-8 encoded)
+
+Parameters:
+    filename (str, optional): The name of the file to be read.
+Usage:
+    You can use this function by calling it with the `filename` parameter
+
+Example:
+    read_file("your_text_file.txt")
+
+Note:
+    - The function uses the 'with' statement to ensure proper file handling.
+    - It doesn't handle exceptions.
+    - This function doesn't require importing any additional modules.
+"""
+
+
+def read_file(filename=""):
+
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
