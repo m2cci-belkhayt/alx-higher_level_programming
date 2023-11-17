c1a1cc883e546633ad269c1cf8106f0a81fb0e81
m2cci-belkhayt <belkhayatetaha1@gmail.com>
2023-11-17 21:35:54 +0100

    still in task 1

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# test_base.py
+"""Defines unittests for base.py.
+"""
+import os
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    
+    def test_id_increment(self):
+        base1 = Base()
+        base2 = Base()
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 2)
+
+    def test_custom_id(self):
+        base_with_id = Base(5)
+        self.assertEqual(base_with_id.id, 5)
