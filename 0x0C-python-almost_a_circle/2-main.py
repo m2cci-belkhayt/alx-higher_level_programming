b8b83afd2638466b85b65ad28b7dfe96baab4f94
m2cci-belkhayt <belkhayatetaha1@gmail.com>
2023-11-17 22:21:57 +0100

    task 5

--- 0x0C-python-almost_a_circle/2-main.py
+++ 0x0C-python-almost_a_circle/2-main.py
@@ -32,3 +32,6 @@ if __name__ == "__main__":
     def display(self):
         for _ in range(self.height):
             print("#" * self.width)
+    
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
