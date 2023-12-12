ec57bcca05a6ac0467f540866f7932e8a36bf255
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2023-12-13 00:10:21 +0100

    third and forth

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+--creates a table called first_table in the current database in your MySQL server.
+--first_table description: id INT, name VARCHAR(256)
+CREATE TABLE if not EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
