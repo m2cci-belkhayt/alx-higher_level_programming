0f38848b8ef250063c0c852e4a21cddd7c61c8c1
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2023-12-13 23:03:33 +0100

    task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Script to create MySQL server user user_0d_1
+-- Create user user_0d_1 with all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
