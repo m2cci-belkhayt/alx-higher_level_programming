f491f00c04c0c35c3edbcd5dc7951b5be3acc014
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2023-12-14 19:50:03 +0100

    comment

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,5 +1,4 @@
---script that displays the number of records with id = 89
--- in the table first table of the database 
+-- Displays the number of records with id = 89 in the table first_table. 
 SELECT count(*)
 FROM `first_table` 
 WHERE `id` = 89;
