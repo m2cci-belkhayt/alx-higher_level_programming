1a40ff796c2d377b0d7311ae4ce73d59e138d9ae
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2023-12-14 21:12:49 +0100

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
 -- all shows contained in hbtn_0d_tvshows.
-USE hbtn_0d_tvshows;
 SELECT tv_shows.title; tv_show_genres.genre_id
 FROM tv_shows JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
