422c6a4882c7678f6506f0f784a8a358c0977b64
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2023-12-14 21:12:17 +0100

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- all shows contained in hbtn_0d_tvshows.
 USE hbtn_0d_tvshows;
 SELECT tv_shows.title; tv_show_genres.genre_id
 FROM tv_shows JOIN tv_show_genres
