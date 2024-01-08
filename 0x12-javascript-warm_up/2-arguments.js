b91c6c79cc8f29cec3594960df7482350a11e018
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-08 19:59:53 +0100

    Create 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// depending on the number of arguments
+// we will write something
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
