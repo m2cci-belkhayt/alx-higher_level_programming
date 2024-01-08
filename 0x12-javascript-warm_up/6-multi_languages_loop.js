8c2756445c28d8d8d64733800ee6a12a0cd65690
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-08 20:36:56 +0100

    Create 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Prints 3 lines using an array of strings and a while loop
+
+const langs = ["C is fun", "Python is cool", "Javascript is amazing"];
+
+let i = 0;
+while (i < 3) {
+  console.log(langs[i]);
+  i++;
+}
