8833a39be519570d781beac2721e0ab9b3d46043
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-13 20:40:27 +0100

    Create 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+def list_states(username, password, database):
+    """
+    Lists all states from the database hbtn_0e_0_usa
+    """
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+    finally:
+        if db:
+            db.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        list_states(sys.argv[1], sys.argv[2], sys.argv[3])
