36b9ef2199b7a50369e5a74e8d633aa9741132b8
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-14 21:27:00 +0100

    Create 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# Changes the name of a State object in the MySQL database hbtn_0e_6_usa.
+# Usage: ./12-model_state_update_id_2.py <mysql username> <mysql password> <database name>
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Check the number of arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Set up the connection to the MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Query the State object with id = 2
+    state_to_update = session.query(State).filter_by(id=2).first()
+
+    # Update the name to "New Mexico" if the state exists
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+
+    # Close the session
+    session.close()
