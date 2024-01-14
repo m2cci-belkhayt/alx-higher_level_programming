3a6e39b73250c8b662eeb1d617eb186a0599581b
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-14 21:25:00 +0100

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,10 +1,37 @@
-Write a script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa
+#!/usr/bin/python3
+# Prints the State object with the specified name from the MySQL database hbtn_0e_6_usa.
+# Usage: ./10-model_state_my_get.py <mysql username> <mysql password> <database name> <state name>
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-Your script should take 4 arguments: mysql username, mysql password, database name and state name to search (SQL injection free)
-You must use the module SQLAlchemy
-You must import State and Base from model_state - from model_state import Base, State
-Your script should connect to a MySQL server running on localhost at port 3306
-You can assume you have one record with the state name to search
-Results must display the states.id
-If no state has the name you searched for, display Not found
-Your code should not be executed when imported
+if __name__ == "__main__":
+    # Check the number of arguments
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Set up the connection to the MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Query the State object with the specified name
+    state_to_search = sys.argv[4]
+    result_state = session.query(State).filter_by(name=state_to_search).first()
+
+    # Display the result
+    if result_state:
+        print(result_state.id)
+    else:
+        print("Not found")
+
+    # Close the session
+    session.close()
