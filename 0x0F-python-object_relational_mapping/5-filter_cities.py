f999a7d7e2e55aaf6166b7a894e02f10212eae91
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-15 20:54:41 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,39 +1,20 @@
 #!/usr/bin/python3
-"""
-Script that lists all cities of a specific state from the database hbtn_0e_4_usa.
-"""
-
+# Displays all cities of a given state from the
+# states table of the database hbtn_0e_4_usa.
+# Safe from SQL injections.
+# Usage: ./5-filter_cities.py <mysql username> \
+#                             <mysql password> \
+#                             <database name> \
+#                             <state name searched>
 import sys
 import MySQLdb
 
-def list_cities(username, password, database, state_name):
-    """
-    Lists all cities of a specific state from the database hbtn_0e_4_usa.
-    """
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-        cursor = db.cursor()
-
-        # Execute a single query to fetch the required data
-        query = """
-                SELECT cities.id, cities.name
-                FROM cities
-                JOIN states ON cities.state_id = states.id
-                WHERE states.name = %s
-                ORDER BY cities.id ASC
-                """
-        cursor.execute(query, (state_name,))
-
-        cities = cursor.fetchall()
-        for city in cities:
-            print(city)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-    finally:
-        if db:
-            db.close()
-
 if __name__ == "__main__":
-    if len(sys.argv) == 5:
-        list_cities(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    
