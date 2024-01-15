ec3f851bc6b756bc7d3323ec6703a462a8c05cc3
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-15 21:06:40 +0100

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-# Prints the first State object from the database hbtn_0e_6_usa.
-# Usage: ./8-model_state_fetch_first.py <mysql username> /
-#                                       <mysql password> /
-#                                       <database name>
+"""
+Script to print the first State object from the hbtn_0e_6_usa database.
+Usage: ./8-model_state_fetch_first.py <mysql username> <mysql password> <database name>
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 if __name__ == "__main__":
+    # Create a connection to the MySQL database using SQLAlchemy
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Retrieve the first State object and print its details
     state = session.query(State).order_by(State.id).first()
     if state is None:
-        print("Nothing")
+        print("Nothing")  # If no State object is found, print "Nothing"
     else:
-        print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))  # Print the details of the first State object
