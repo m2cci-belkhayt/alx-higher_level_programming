42be6e5673990de3ddbf5d3b28ab9a970f0d169b
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-15 20:58:56 +0100

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
-# Lists all State objects containing the letter 'a' from the MySQL database hbtn_0e_6_usa.
-# Usage: ./9-model_state_filter_a.py <mysql username> <mysql password> <database name>
+# Lists all State objects that contain the letter a
+# from the database hbtn_0e_6_usa.
+# Usage: ./9-model_state_filter_a.py <mysql username> /
+#                                    <mysql password> /
+#                                    <database name>
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
-    # Check the number of arguments
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Set up the connection to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
-    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
-
-    # Create a Session
     session = Session()
 
-    # Query and display State objects containing the letter 'a', ordered by id
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
-
-    for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
-
-    # Close the session
-    session.close()
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
