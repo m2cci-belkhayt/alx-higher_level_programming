c73b2e6b3fb492420087de53cea62870d2e6481a
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-15 21:18:05 +0100

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-# Lists all City objects from the database hbtn_0e_14_usa.
-# Usage: ./14-model_city_fetch_by_state.py <mysql username> /
-#                                          <mysql password> /
-#                                          <database name>
+"""
+Lists all City objects from the database hbtn_0e_14_usa.
+Usage: ./14-model_city_fetch_by_state.py <mysql username> <mysql password> <database name>
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,12 +11,18 @@ from model_state import State
 from model_city import City
 
 if __name__ == "__main__":
+    # Set up the connection to the MySQL server
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+    
+    # Create a Session
     session = Session()
 
+    # Query and print City objects along with their corresponding State names
     for city, state in session.query(City, State) \
                               .filter(City.state_id == State.id) \
                               .order_by(City.id):
