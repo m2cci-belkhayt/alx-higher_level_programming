e8d159dbe28eaec2b7e7e57980ea2b0ff2be059b
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-18 11:00:13 +0100

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,7 @@ def list_states_N(username, password, database):
                              passwd=password, db=database)
         cursor = db.cursor()
         cursor.execute("SELECT * FROM states "
-                       "WHERE name LIKE 'N%' COLLATE utf8_general_ci "
+                       "WHERE name LIKE 'N%' "
                        "ORDER BY id ASC")
 
         states = cursor.fetchall()
