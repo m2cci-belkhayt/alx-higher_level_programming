ad95648cd693d1447004a08e8270fa15e4adc2bb
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2024-01-18 11:30:35 +0100

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,43 +1,19 @@
 #!/usr/bin/python3
-"""
-Script that takes in the name of a state as an argument
-and lists all cities of that state, using the database hbtn_0e_4_usa
-"""
+"""Lists states"""
 
-import sys
 import MySQLdb
-
-
-def list_cities(username, password, database, state_name):
-    """
-    Lists all cities of a given state from the database hbtn_0e_4_usa
-    """
-    try:
-        db = MySQLdb.connect(host="localhost",
-                             port=3306, user=username,
-                             passwd=password, db=database)
-        cursor = db.cursor()
-
-        query = (
-            "SELECT cities.id, cities.name, states.name "
-            "FROM cities "
-            "JOIN states ON cities.state_id = states.id "
-            "WHERE states.name = %s "
-            "ORDER BY cities.id ASC"
-        )
-
-        cursor.execute(query, (state_name,))
-        cities = cursor.fetchall()
-
-        for city in cities:
-            print(city)
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-    finally:
-        if db:
-            db.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) == 5:
-        list_cities(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """, (argv[4], ))
+    print(", ".join(map(lambda x: x[0], cur.fetchall())))
+    cur.close()
+    conn.close()
